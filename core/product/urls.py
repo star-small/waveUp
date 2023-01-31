4e53e2632c59b4e509dd0ceaa7dfcf26bbf2d6a6
star-small <starsmall890@gmail.com>
2023-01-31 10:57:39 +0500

    media404fix

--- core/product/urls.py
+++ core/product/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path
 from . import views
 urlpatterns = [
@@ -5,3 +7,4 @@ urlpatterns = [
     path("filter_by_price", views.filter_by_price)
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
