f2797a038286edc04439d8d33a50312f708bbc76
star-small <starsmall890@gmail.com>
2023-03-04 15:55:22 +0500

    jinja-refacrot

--- core/product/modules/mail.py
+++ core/product/modules/mail.py
@@ -1,7 +1,6 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import datetime
 from django.utils import timezone
 # Email parameters
 to_addr = 'root@localhost'
--- core/product/views.py
+++ core/product/views.py
@@ -1,46 +1,16 @@
-from pathlib import Path
-import csv
 import os
-import datetime
+
 from django.shortcuts import render, redirect
-# from django.contrib.auth.models import User
-from .models import Product, Category
-# from .forms import UserForm
-from django.utils.timezone import now
-from .modules.sheets import write_table
+
 from .modules.mail import send_mail
+# from django.contrib.auth.models import User
+# from .forms import UserForm
+from .modules.sheets import write_table
+from .modules.utils import *
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-# Create your views here.
 
 
-def dict_to_db(values_dict):
-    for values in values_dict:
-        category = values['category']
-        values['date'] = now() if values['date'] == '' else datetime.datetime.strptime(
-            values['date'], "%d %m %Y %H:%M")
-        if not Category.objects.filter(name=category).exists() and category != '':
-            Category.objects.create(name=category)
-        values['category'] = None if category == '' else Category.objects.get(
-            name=category)
-        select = list(map(list, values.items()))
-        select = dict(select[:5] + select[6:])
-        existing_product = Product.objects.filter(
-            from_csv=True, **select).first()
-        print(existing_product)
-        if not existing_product:
-            Product.objects.create(**values, from_csv=True)
-
-
-def read_from_csv():
-    with open(BASE_DIR/"files/file.csv", newline='', encoding="utf-8-sig") as file:
-        rows = list(csv.reader(file, dialect='excel',
-                    delimiter=';', quotechar='|'))
-        model_fields = Product._meta.get_fields()[1:-2]
-        field_values = []
-        for row in rows[1:]:
-            field_values.append(
-                dict([(model_fields[i].name, row[i]) for i in range(len(row))]))
-    return field_values
 
 
 def show_main(request):
